53389a4fdb03885cda892cd33e7c1ad963a42c00
RasoliDavide <rasoli.davide@iisgalvanimi.edu.it>
2019-02-06 14:01:56 +0100

    6.1 Finito

--- angular-Reddit-Rasoli/src/app/article/article.component.ts
+++ angular-Reddit-Rasoli/src/app/article/article.component.ts
@@ -1,30 +1,28 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-
+import { Article } from './article.model';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
+
+
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'card';
-  votes: number;
-  title: string;
-  link: string;
+  article:Article
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article('Angular 2','http://angular.io',10);
   }
 
-  voteUp():Boolean {
-    this.votes += 1;
-	return false;
+  voteUp(): Boolean {
+    this.article.voteUp(); 
+    return false;
   }
 
   voteDown():Boolean {
-  this.votes -= 1;
-  return false;
+    this.article.voteDown(); 
+    return false;
   }
 
   ngOnInit() {}
